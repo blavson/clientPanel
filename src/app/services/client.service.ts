5dc96b2c8f7b37d34eeb752aadcb7d76871ebeac
mblavatsky-at-222784684405 <mblavatsky@guesswho.com>
2019-09-30 14:26:05 +0400

    inital stage

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+  constructor() {}
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCRzJpkXPiDey99x165p5eTZqrS23781rk',
+    authDomain: 'clienpanelprod-f198f.firebaseapp.com',
+    databaseURL: 'https://clienpanelprod-f198f.firebaseio.com',
+    projectId: 'clienpanelprod-f198f',
+    storageBucket: 'clienpanelprod-f198f.appspot.com',
+    messagingSenderId: '878730737602',
+    appId: '1:878730737602:web:9cff47319008d6f0af085b'
+  }
 };
 
 /*
